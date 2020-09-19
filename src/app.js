6477c151e87ca0898aa6deb8719c3ce56a2104ab
jholkin <jhil.palacios@unas.edu.pe>
2020-09-19 18:11:38 -0500

    retirement v1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// Routes
+const retiremetRouter = require('./routes/retirement');
+
 // Configuration
 const port = 3000;
 
@@ -8,16 +11,12 @@ const port = 3000;
 // Middleware
 app.use(express.json());
 
-// Routes
-const retiremetRouter = require('./routes/retirement');
-
-
 // Functions
 app.get('/', (req, res) => {
     res.send('Hello Microservices');
 });
 
-app.get('api/retirement', retiremetRouter);
+app.use('/api/retirement', retiremetRouter);
 
 app.listen(port, () => {
     console.log(`Server run in port ${port}`);
