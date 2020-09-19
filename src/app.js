c16beeba3844fe8d8517157beb36a548568e0533
jholkin <jhil.palacios@unas.edu.pe>
2020-09-19 16:55:57 -0500

    structure project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 
+// Configuration
 const port = 3000;
 
+
+// Middleware
+app.use(express.json());
+
+// Routes
+const retiremetRouter = require('./routes/retirement');
+
+
+// Functions
 app.get('/', (req, res) => {
     res.send('Hello Microservices');
 });
+
+app.get('api/retirement', retiremetRouter);
 
 app.listen(port, () => {
     console.log(`Server run in port ${port}`);
