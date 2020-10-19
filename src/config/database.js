1ed0248747c19aca9fc91451ebd56888c0685fb1
jholkin <jhil.palacios@unas.edu.pe>
2020-10-19 00:23:49 -0500

    pulling connection to database

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,9 @@
  const moogose = require("mongoose");
 
- moogose.connect("mongodb://localhost:27017/transactions").then(db => console.log('Db is connected', db.connection.host));
+ moogose.connect("mongodb://localhost:27017/transactions",{
+     useCreateIndex: true,
+     useFindAndModify: false,
+     useNewUrlParser: true
+ }).then(db => console.log('Db is connected', db.connection.host));
 
  exports.module = moogose;
